db3619336618ec782429351374f10d1dfa4d4b80
Loggistik <mister.bondarenko-miha2015@yandex.ru>
2022-01-31 16:57:59 +0300

    Try 1

---
+++ Task_1/Task_1.cpp
@@ -0,0 +1,98 @@
+﻿#include <iostream>
+
+class Vector {
+public:
+
+    Vector() 
+    {
+        x = 0;
+        y = 0;
+        z = 0;
+    }
+
+    Vector(float x, float y, float z)
+    {
+        this->x = x;
+        this->y = y;
+        this->z = z;
+    }
+
+    operator float() 
+    {
+        return sqrt(x * x + y * y + z * z);
+    }
+
+    float operator[](int index)
+    {
+        switch (index)
+        {
+        case 0:
+            return x;
+            break;
+
+        case 1:
+            return y;
+            break;
+
+        case 2:
+            return z;
+            break;
+
+        default:
+            return 0;
+            break;
+        }
+    }
+
+    friend Vector operator+(const Vector& a, const Vector& b);
+    friend std::ostream& operator<<(std::ostream& out, const Vector& v);
+    friend Vector operator*(const Vector& a, const float& b);
+    friend Vector operator-(const Vector& a, const Vector& b);
+    friend std::istream& operator>>(std::istream& in, Vector& v);
+
+private:
+    float x;
+    float y;
+    float z;
+};
+
+Vector operator+(const Vector& a, const Vector& b)
+{
+    return Vector(a.x + b.x, a.y + b.y, a.z + b.z);
+}
+
+std::ostream& operator<<(std::ostream& out, const Vector& v)
+{
+    out << ' ' << v.x << " " << v.y << " " << v.z;
+    return out;
+}
+
+Vector operator*(const Vector& a, const float& b)
+{
+    return Vector(a.x * b, a.y * b, a.z * b);
+}
+
+Vector operator-(const Vector& a, const Vector& b) 
+{
+    return Vector(a.x - b.x, a.y - b.y, a.z - b.z);
+}
+
+std::istream& operator>>(std::istream& in, Vector& v) 
+{
+    in >> v.x >> v.y >> v.z;
+    return in;
+}
+
+int main()
+{
+    Vector v1 = Vector(1, 1, 1);
+    Vector v2 = v1 * 2.0f;
+
+    std::cout << "v1:" << v1 << "\n";
+    std::cout << "v2:" << v2 << "\n";
+    std::cout << v2 - v1 << "\n";
+    std::cin >> v2;
+    std::cout << "v:" << v2;
+
+}
+
